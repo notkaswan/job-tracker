846de47725f7f6a04edf6c0929df8259d5f838dd
Deepak Kaswan <62922598+notkaswan@users.noreply.github.com>
2025-12-18 01:44:42 +0530

    new file:   src/utils/jobs.js

---
+++ src/utils/jobs.js
@@ -0,0 +1,45 @@
+import { app } from "./firebase";
+import {
+  addDoc,
+  collection,
+  deleteDoc,
+  doc,
+  getDoc,
+  getDocs,
+  getFirestore,
+  orderBy,
+  query,
+  updateDoc,
+  where,
+} from "firebase/firestore";
+const db = getFirestore(app);
+const jobsCol = collection(db, "jobs");
+
+export async function addJob(job) {
+  const docRef = await addDoc(jobsCol, job);
+  return docRef.id;
+}
+
+export async function updateJob(id, data) {
+  const ref = doc(db, "jobs", id);
+  await updateDoc(ref, data);
+}
+
+export async function deleteJob(id) {
+  await deleteDoc(doc(db, "jobs", id));
+}
+
+export async function getJob(id) {
+  const snap = await getDoc(doc(db, "jobs", id));
+  return snap.exists() ? { id: snap.id, ...snap.data() } : null;
+}
+
+export async function getJobsForUser(userId) {
+  const q = query(
+    jobsCol,
+    where("userId", "==", userId),
+    orderBy("createdAt", "dec")
+  );
+  const snap = await getDocs(q);
+  return snap.docs.map((d) => ({ id: d.id, ...d.data() }));
+}
